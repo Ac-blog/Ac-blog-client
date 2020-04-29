13b4e1c3b8b82338c7ab5d4a71348cf9d9178f0d
AllenChinese <13402202685@163.com>
2020-04-29 17:00:14 +0800

    feat: 用户信息组件

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,15 @@
+/** 文章卡片 */
 import ArticleCard from './ArticleCard.vue'
+/** 文章分类标签 */
 import ArticleTag from './ArticleTag.vue'
+/** 文章标签分类容器 */
 import TagsBox from './TagsBox.vue'
+/** 个人信息 */
+import UserProfile from './UserProfile.vue'
 
 export {
   ArticleCard,
   ArticleTag,
-  TagsBox
+  TagsBox,
+  UserProfile
 }
